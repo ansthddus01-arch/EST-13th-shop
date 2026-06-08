3bdd0d89edb6ace463a34e77c8559c479c4a64ea
ansthddus01-arch <ansthddus01@gmail.com>
2026-06-09 01:19:11 +0900

    feat: cart.js 상품출력하기 과제 업로드 및 common.js에 cart.push price추가

--- js/utils/common.js
+++ js/utils/common.js
@@ -45,6 +45,7 @@ export function addToCart(product, qty = 1) {
       brand: product.brand,
       thumb: product.thumbnail,
       qty: qty,
+      price: product.price,
     });
   }
   writeCart(cart);
